0d65fbef9c497b16204abfcee9b2a2f571e8010c
John Rey <adioskid@mail.ru>
2023-08-16 03:14:54 +0300

    log

--- src/main/java/ru/hogwarts/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -1,24 +1,49 @@
 package ru.hogwarts.school.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.service.AvatarService;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 @RestController
 public class AvatarController {
     @Autowired
     private AvatarService avatarService;
 
-    @PostMapping(value = "/cover", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "cover", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> upload(@RequestParam Long id, @RequestParam MultipartFile cover) throws IOException {
         avatarService.upload(id, cover);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("pic_bd/{imageId}")
+    public ResponseEntity<byte[]> getImage(@PathVariable Long imageId) {
+        Avatar avatar = avatarService.avatar(imageId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(avatar.getMediaType()));
+        headers.setContentLength(avatar.getData().length);
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(avatar.getData());
+    }
+
+
+    @GetMapping("pic_hd")
+    public void getPicHd(@RequestParam long id, HttpServletResponse response) throws IOException {
+        Avatar avatar = avatarService.avatar(id);
+        try (FileInputStream fileInputStream = new FileInputStream(avatar.getFilePath());
+             OutputStream outputStream = response.getOutputStream()) {
+            response.setContentType(avatar.getMediaType());
+            response.setContentLength((int) avatar.getFileSize());
+            fileInputStream.transferTo(outputStream);
+        }
     }
 }
--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -3,17 +3,17 @@ package ru.hogwarts.school.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Students;
 import ru.hogwarts.school.repositories.AvatarRepository;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Objects;
 
 @Service
+@Transactional
 public class AvatarService {
     @Value("${path.my.photo}")
     String path;
@@ -22,6 +22,11 @@ public class AvatarService {
     @Autowired
     private MetCrud<Students> metCrud;
 
+
+    public Avatar avatar(long id) {
+        return avatarRepository.findByStudentsId(id).orElse(null);
+    }
+
     private static String point(String txt) {
         return txt.substring(txt.indexOf('.'));
     }
@@ -29,20 +34,20 @@ public class AvatarService {
     public void upload(Long id, MultipartFile multipartFile) throws IOException {
         Students students = metCrud.find(id);
         String pathFile = path + point(Objects.requireNonNull(multipartFile.getOriginalFilename()));
-        InputStream inputStream = multipartFile.getInputStream();
-        inputStream.transferTo(new FileOutputStream(pathFile));
         Avatar avatar = avatarRepository.findByStudentsId(id).orElse(new Avatar());
-
         avatar.setStudents(students);
-        avatar.setFilePath(path);
+        avatar.setFilePath(pathFile);
         avatar.setFileSize(multipartFile.getSize());
         avatar.setMediaType(multipartFile.getContentType());
-        avatar.setData(inputStream.readAllBytes());
-        avatarRepository.save(avatar);
-
+        avatar.setData(multipartFile.getBytes());
+        if (students != null) {
+            avatarRepository.save(avatar);
+        } else {
+            throw new NullPointerException("Такого ученика нет!");
+        }
     }
-
-
 }
+
+
 
 
